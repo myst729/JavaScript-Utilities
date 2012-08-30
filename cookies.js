1e580c92ff6f436ba19d6b686bbf3dda5c86b81c
Leo Deng <myst.dg@gmail.com>
2012-08-30 17:56:12 +0800

    initial

---
+++ cookies.js
@@ -0,0 +1,37 @@
+/* JavaScript Cookies */
+
+function CookieHandler() {
+
+    this.setCookie = function (name, value, seconds) {
+ 
+        if (typeof(seconds) != 'undefined') {
+            var date = new Date();
+            date.setTime(date.getTime() + (seconds*1000));
+            var expires = "; expires=" + date.toGMTString();
+        }
+        else {
+            var expires = "";
+        }
+
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+
+    this.getCookie = function (name) {
+
+        name = name + "=";
+        var carray = document.cookie.split(';');
+
+        for(var i=0;i < carray.length;i++) {
+            var c = carray[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        }
+
+        return null;
+    }
+
+    this.deleteCookie = function (name) {
+        this.setCookie(name, "", -1);
+    }
+
+}
