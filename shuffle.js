98eba0f26e580ff690be93b5b96b4d3ae939bd49
Leo Deng <myst729@users.noreply.github.com>
2014-11-09 18:56:40 +0800

    Create shuffle.js

---
+++ shuffle.js
@@ -0,0 +1,17 @@
+// Fisher Yates Shuffle Algorithm
+
+Array.prototype.shuffle = function() {
+  var i = this.length
+  var j
+  var temp
+
+  if(i > 1) {
+    while(--i) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = this[i]
+      this[i] = this[j]
+      this[j] = temp
+    }
+  }
+  return this
+}
